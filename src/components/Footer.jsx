155f7701a7ba0eac0f4b4a4cf4d23de385fa4aa3
VENSROY <venugopljangid01@gmail.com>
2026-03-04 18:05:58 +0530

    Updated contact section and added social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-earthy-brown text-ivory py-8 text-center">
-      © 2021 ANMOL Art. All Rights Reserved.
+      © 2006 ANMOL Art. All Rights Reserved.
     </footer>
   );
 }
